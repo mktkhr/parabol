bbe7764c74d9edfe3f06def1b76e2a5ccfef617b
takahiro.maki <j39g8bxh@gmail.com>
2026-09-16 02:24:16 +0900

    fix(oidc): hand openid-client a native Response despite the global fetch patch

--- packages/server/utils/oidc/getOIDCClientConfig.ts
+++ packages/server/utils/oidc/getOIDCClientConfig.ts
@@ -1,12 +1,19 @@
 import * as client from 'openid-client'
 import type {OIDCEnv} from './getOIDCEnv'
+import oidcFetch from './oidcFetch'
 
 let configPromise: Promise<client.Configuration> | null = null
 
 export const getOIDCClientConfig = (env: OIDCEnv) => {
   if (!configPromise) {
     configPromise = client
-      .discovery(new URL(env.issuer), env.clientId, env.clientSecret)
+      .discovery(new URL(env.issuer), env.clientId, env.clientSecret, undefined, {
+        [client.customFetch]: oidcFetch
+      })
+      .then((config) => {
+        config[client.customFetch] = oidcFetch
+        return config
+      })
       .catch((error) => {
         configPromise = null
         throw error
---
+++ packages/server/utils/oidc/oidcFetch.ts
@@ -0,0 +1,18 @@
+import type {CustomFetchOptions} from 'openid-client'
+
+const oidcFetch = async (url: string, options: CustomFetchOptions) => {
+  const upstream = await fetch(url, {
+    method: options.method,
+    headers: options.headers,
+    body: options.body as BodyInit | null | undefined,
+    redirect: options.redirect,
+    signal: options.signal
+  })
+  return new Response(await upstream.arrayBuffer(), {
+    status: upstream.status,
+    statusText: upstream.statusText,
+    headers: new Headers([...upstream.headers.entries()])
+  })
+}
+
+export default oidcFetch
